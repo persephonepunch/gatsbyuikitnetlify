31c0ed4c5a7bd32c7f652e34a3e554723b39e6fa
ysl150 <yoonsun.lee@tescometering.com>
2018-05-24 11:40:12 -0400

    working with spans in tile component

--- src/components/Blocks/_Dashboard.js
+++ src/components/Blocks/_Dashboard.js
@@ -6,63 +6,45 @@ export class Dashboard extends Component {
       <div className="uk-section ">
         <div className="uk-container uk-container-medium">
           <div
-            className="uk-child-width-1-1@s uk-grid-collapse uk-text-center uk-box-shadow-large "
+            className="uk-child-width-1-3@s uk-grid-collapse uk-text-center uk-box-shadow-large"
             data-uk-grid
             data-uk-scrollspy="target: p; cls:uk-animation-fade; delay: 500"
           >
-           
-
-
-
-
-
-
-
-
-          
             <div>
-              <div className="uk-tile uk-tile-secondary">
-                <h4 className="uk-h4">Placeholder</h4>
+              <div className="uk-tile uk-tile-primary ">
+                <h4 className="uk-h4">Placeholderg</h4>
                 <p>
-               Placeholder for the Widget
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                  ornare massa felis, sit amet placerat nisi cursus a. Cras diam
+                  turpis, sodales ac ullamcorper vel, lobortis ut tellus.
+                  Curabitur ut iaculis dolor.
                 </p>
               </div>
             </div>
-
-            <div></div>
-
-
-             
-
-
-          </div>
-
-
-          <div
-            className="uk-child  uk-grid-collapse  uk-box-shadow-large"
-            data-uk-grid
-            data-uk-scrollspy="target: p; cls:uk-animation-fade; delay: 500"
-          >
-           
-          
-            <div  className="uk-child  uk-grid-collapse  uk-box-shadow-large">
+            
+            <div>
               <div className="uk-tile uk-tile-default">
-                <h4 className="uk-h4">Second Panel</h4>
+                <h4 className="uk-h4"> </h4>
                 <p>
-               Placeholder for the Hero App
+                
                 </p>
               </div>
             </div>
-
-
-             
-
-
+            <div>
+              <div className="uk-tile uk-tile-default">
+                <h4 className="uk-h4"> </h4>
+                <p>
+                  
+                </p>
+              </div>
+            </div>
+           
           </div>
         </div>
       </div>
     );
   }
 }
+
 
 export default Dashboard;
