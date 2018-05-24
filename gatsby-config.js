94d43c2a141bb9cacf142ebe15f01884c6f05626
yoonsun lee <yoonsunlee150@gmail.com>
2018-05-23 21:45:56 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Tempest,
+    title: 'Gatsby Tempest',
   },
   pathPrefix: "/gatsbyuikit.git",
   plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
