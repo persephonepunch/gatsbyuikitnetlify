dc2190daa9beb9361dcefde0330e3588db7417e1
yoonsun lee <yoonsunlee150@gmail.com>
2018-06-04 11:21:23 -0400

    Update _NewsLetter.js

--- src/components/Blocks/_NewsLetter.js
+++ src/components/Blocks/_NewsLetter.js
@@ -7,7 +7,7 @@ export class NewsLetter extends Component {
         <div className="uk-container">
           <h3 className="uk-flex uk-flex-center">Subscribe</h3>
 
-          <form className="uk-flex uk-flex-center">
+          <form className="uk-flex uk-flex-center" name= "signup" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
             <input
               className="uk-input uk-width-1-2@s"
               type="text"
