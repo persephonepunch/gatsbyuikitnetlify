50f7d537831e12c68e4da49ca824245a8c9a02ec
yoonsun lee <yoonsunlee150@gmail.com>
2018-06-04 11:47:41 -0400

    form syntax corrections

--- src/components/Blocks/_NewsLetter.js
+++ src/components/Blocks/_NewsLetter.js
@@ -7,13 +7,13 @@ export class NewsLetter extends Component {
         <div className="uk-container">
           <h3 className="uk-flex uk-flex-center">Subscribe</h3>
 
-          <form className="uk-flex uk-flex-center" name= "signup" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+          <form className="uk-flex uk-flex-center" name= "signup" method="POST" netlify data-netlify="true" data-netlify-honeypot="bot-field">
             <input
               className="uk-input uk-width-1-2@s"
               type="text"
               placeholder="Email"
             />
-       <button className="uk-button-primary">SUBMIT</button>
+       <button type="submit" className="uk-button-primary">SUBMIT</button>
           </form>
         </div>
       </div>
