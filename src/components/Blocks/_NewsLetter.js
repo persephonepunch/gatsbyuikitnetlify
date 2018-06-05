ce61d2dca89652979a0d563fbd038b7165ecd1b3
yoonsun lee <yoonsunlee150@gmail.com>
2018-06-05 19:45:42 -0400

    Update _NewsLetter.js

--- src/components/Blocks/_NewsLetter.js
+++ src/components/Blocks/_NewsLetter.js
@@ -8,17 +8,17 @@ export class NewsLetter extends Component {
           <h3 className="uk-flex uk-flex-center">Subscribe</h3>
 
           <form className="uk-flex uk-flex-center" 
-              name = "signup" 
-              method="POST" netlify 
+              name = "contact-form" 
+              method="post"  
               data-netlify="true" 
               data-netlify-honeypot="bot-field">
             <input
               className="uk-input uk-width-1-2@s"
               name="email"
-              type="text"
+              type="email"
               placeholder="Email"
             />
-       <button type="submit" className="uk-button-primary">SUBMIT</button>
+       <button className="uk-button-primary">SUBMIT</button>
           </form>
         </div>
       </div>
